a4cf1b22a63dcd3a66b2de40ac00555cd3ea8817
luis torres <tl224627@gmail.com>
2020-10-30 23:34:22 -0600

    intial

--- EtchASketch/EtchASketchForm.Designer.vb
+++ EtchASketch/EtchASketchForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class EtchASketchForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,19 +20,179 @@ Partial Class EtchASketchForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.drawbox = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.SelectColorButton = New System.Windows.Forms.Button()
+        Me.DrawWaveformsButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DrawWaveformsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.drawbox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'drawbox
+        '
+        Me.drawbox.Location = New System.Drawing.Point(12, 85)
+        Me.drawbox.Name = "drawbox"
+        Me.drawbox.Size = New System.Drawing.Size(983, 701)
+        Me.drawbox.TabIndex = 0
+        Me.drawbox.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 122)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(983, 664)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'SelectColorButton
+        '
+        Me.SelectColorButton.Location = New System.Drawing.Point(48, 851)
+        Me.SelectColorButton.Name = "SelectColorButton"
+        Me.SelectColorButton.Size = New System.Drawing.Size(182, 71)
+        Me.SelectColorButton.TabIndex = 2
+        Me.SelectColorButton.Text = "Select Color"
+        Me.SelectColorButton.UseVisualStyleBackColor = True
+        '
+        'DrawWaveformsButton
+        '
+        Me.DrawWaveformsButton.Location = New System.Drawing.Point(236, 851)
+        Me.DrawWaveformsButton.Name = "DrawWaveformsButton"
+        Me.DrawWaveformsButton.Size = New System.Drawing.Size(182, 71)
+        Me.DrawWaveformsButton.TabIndex = 3
+        Me.DrawWaveformsButton.Text = "DrawWave forms"
+        Me.DrawWaveformsButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(424, 851)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(182, 71)
+        Me.ClearButton.TabIndex = 4
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(623, 851)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(182, 71)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(32, 32)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1018, 48)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(72, 36)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(359, 44)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectColorToolStripMenuItem, Me.DrawWaveformsToolStripMenuItem, Me.ClearToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(75, 36)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'SelectColorToolStripMenuItem
+        '
+        Me.SelectColorToolStripMenuItem.Name = "SelectColorToolStripMenuItem"
+        Me.SelectColorToolStripMenuItem.Size = New System.Drawing.Size(329, 44)
+        Me.SelectColorToolStripMenuItem.Text = "Select Color"
+        '
+        'DrawWaveformsToolStripMenuItem
+        '
+        Me.DrawWaveformsToolStripMenuItem.Name = "DrawWaveformsToolStripMenuItem"
+        Me.DrawWaveformsToolStripMenuItem.Size = New System.Drawing.Size(329, 44)
+        Me.DrawWaveformsToolStripMenuItem.Text = "Draw Waveforms"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(329, 44)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(85, 36)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(214, 44)
+        Me.AboutToolStripMenuItem.Text = "About"
         '
         'EtchASketchForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1018, 943)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DrawWaveformsButton)
+        Me.Controls.Add(Me.SelectColorButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.drawbox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "EtchASketchForm"
         Me.Text = "Etch-A-Sketch"
+        CType(Me.drawbox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents drawbox As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents SelectColorButton As Button
+    Friend WithEvents DrawWaveformsButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DrawWaveformsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
 End Class
--- EtchASketch/EtchASketchForm.vb
+++ EtchASketch/EtchASketchForm.vb
@@ -5,9 +5,167 @@
 'Etch-A-Sketch
 'https://github.com/LuisTorres0419/EtchASketch.git
 
+Option Strict On
+Option Explicit On
 
 Public Class EtchASketchForm
 
+    Dim drawPen As New System.Drawing.Pen(Color.Black, 1)
+    Dim erasePen As New System.Drawing.Pen(Color.FromName("control"), 10)
+    Dim sinePen As New Pen(Color.Black, 10)
+    Dim g As System.Drawing.Graphics
+    Dim lastX, lastY As Integer
 
+
+    Sub Draw(x As Integer, y As Integer)
+
+        g = drawbox.CreateGraphics
+
+        If lastX = Nothing Then
+            g.DrawLine(drawPen, x, y, x, y)
+        Else
+            g.DrawLine(drawPen, x, y, lastX, lastY)
+        End If
+
+        lastX = x
+        lastY = y
+
+    End Sub
+
+    Sub Eraser(x As Integer, y As Integer)
+
+        g = drawbox.CreateGraphics
+
+        If lastX = Nothing Then
+            g.DrawLine(drawPen, x, y, x, y)
+        Else
+            g.DrawLine(drawPen, x, y, lastX, lastY)
+        End If
+
+        lastX = x
+        lastY = y
+
+    End Sub
+
+    Sub PictureBox1_Click(sender As Object, e As MouseEventArgs) Handles drawbox.MouseDown, drawbox.MouseMove
+        ActiveControl = drawbox
+        If e.Button.ToString = "left" Then
+            Draw(e.X, e.Y)
+        ElseIf e.Button.ToString = "left" Then
+            Eraser(e.X, e.Y)
+        ElseIf e.Button.ToString = "Middle" Then
+            ColorChooser()
+        End If
+    End Sub
+
+    Sub ColorChooser()
+        ColorChoose.ShowDialog()
+        drawPen.Color = ColorChoose.Color
+    End Sub
+
+    Sub Clear()
+
+        For i = 1 To 100
+            drawbox.Left = drawbox.Left + 5
+            drawbox.Top = drawbox.Top + 5
+            drawbox.Left = drawbox.Left - 10
+            drawbox.Top = drawbox.Top - 10
+            drawbox.Left = drawbox.Left + 10
+            drawbox.Top = drawbox.Top + 10
+            drawbox.Left = drawbox.Left - 5
+            drawbox.Top = drawbox.Top - 5
+        Next
+
+        If g IsNot Nothing Then
+            g.Clear(Color.FromName("control"))
+        End If
+
+    End Sub
+
+    Private Sub drawbox_MouseUp(sender As Object, e As MouseEventArgs) Handles drawbox.MouseUp
+        lastX = 0
+        lastY = 0
+    End Sub
+
+
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click, ClearToolStripMenuItem.Click
+        Clear()
+    End Sub
+
+    Private Sub SelectColorButton_Click(sender As Object, e As EventArgs) Handles SelectColorButton.Click, SelectColorToolStripMenuItem.Click
+        ColorChooser()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click, EditToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub DrawWaveformsButton_Click(sender As Object, e As EventArgs) Handles DrawWaveformsButton.Click
+
+        If g IsNot Nothing Then
+            g.Clear(Color.FromName("Control"))
+        End If
+
+        DrawWaveform()
+
+    End Sub
+
+    Private Sub EtchASketchForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ActiveControl = drawbox
+    End Sub
+
+    Sub DrawWaveform()
+        Dim SinPen As New System.Drawing.Pen(Color.Black, 3)
+        Dim CoSinPen As New System.Drawing.Pen(Color.Red, 3)
+        Dim TangentPen As New System.Drawing.Pen(Color.Blue, 3)
+        Dim LinePen As New System.Drawing.Pen(Color.Black, 3)
+        Dim x As Double
+        Dim y As Double
+        g = drawbox.CreateGraphics
+
+        For i = 1 To 10
+            g.DrawLine(LinePen, 62 * i, 500, 62 * i, -500)
+            g.DrawLine(LinePen, 1000, 34 * i, -1000, 34 * i)
+        Next
+
+        For Cycles As Double = 0 To 1000
+
+            y = Math.Sin(Cycles / 400 * 2 * Math.PI) * 100 + 150
+            x = Cycles
+            g.DrawLine(SinPen, CType(x, Single), CType(y, Single), CType(x, Single) + 1, CType(y, Single))
+
+        Next
+
+        For Cycles As Double = 0 To 100
+
+            y = Math.Sin(Cycles / 400 * 2 * Math.PI) * 100 + 150
+            x = Cycles
+            g.DrawLine(SinPen, CType(x, Single), CType(y, Single), CType(x, Single) + 1, CType(y, Single))
+
+        Next
+
+        For Cycles As Double = 0 To 100
+
+            y = Math.Sin(Cycles / 400 * 2 * Math.PI) * 20 + 150
+            x = Cycles
+
+            If lastX = Nothing Then
+                lastX = CInt(x)
+                lastY = CInt(y)
+            ElseIf y - lastY < -50 Then
+                lastX = CInt(x)
+                lastY = CInt(y)
+            End If
+
+            g.DrawLine(SinPen, CType(x, Single), CType(y, Single), lastX, lastY)
+
+            lastX = CInt(x)
+            lastY = CInt(y)
+
+        Next
+
+    End Sub
 
 End Class
